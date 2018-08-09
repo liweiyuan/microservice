5598f538a8bf0fef6b9c89001c09f08852cc2299
liweiyuan <liweiyuan@etouch.cn>
2018-08-09 13:16:02 +0800

    enhancement：初始化日志

--- main.go
+++ main.go
@@ -4,29 +4,40 @@ import (
 	httptransport "github.com/go-kit/kit/transport/http"
 	"microservice/transport"
 	"net/http"
-	"log"
 	"fmt"
 	"microservice/service"
 )
 
 func main() {
-	svc := service.StringServiceImpl{}
+
+	//log
+	//logger := log.NewLogfmtLogger(os.Stderr)
+
+	var svc service.StringService
+	svc = service.StringServiceImpl{} //接口与实现类的调用
+
+	//svc = loggingMiddleware{logger, svc}
+
 	uppercaseHandler := httptransport.NewServer(
 		transport.MakeUppercaseEndpoint(svc),
 		transport.DecodeUppercaseRequest,
 		transport.EncodeResponse,
 	)
+
 	countHandler := httptransport.NewServer(
 		transport.MakeCountEndpoint(svc),
 		transport.DecodeCountRequest,
 		transport.EncodeResponse,
 	)
+
 	fmt.Printf("listener to the port : 8080")
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", nil)
+
+	//日志打印
+	//logger.Log("msg", "HTTP", "addr", "8080")
+	//logger.Log("err", http.ListenAndServe("8080", nil))
 
 }
-
-
